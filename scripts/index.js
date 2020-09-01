70c30abb9062516aff55492d9f282ec784911d60
Jordi Garcia <jordi@tokyoplay.com>
2020-09-01 13:53:29 -0500

    funcionalidad de boton de menu y esconder sing button

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
 let isMenuOpen = false;
 function showMenu(){
     let menumorado= document.getElementById('menumorado');
+    let sButton = document.getElementById('signButton');
     if(!isMenuOpen){
         menumorado.style.height = "auto";
+        menumorado.style.padding= "15px";
+        sButton.style.display= "inline-block";
     }else{
         menumorado.style.height = "0px";
+        menumorado.style.padding= "0px";
+        sButton.style.display= "none";
     }
     isMenuOpen=!isMenuOpen;
 }
